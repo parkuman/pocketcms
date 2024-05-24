6ffe6500c6667d4f7bba11296d9f183efd00699f
Mohammed Alamri <modev32@gmail.com>
2024-05-24 23:59:51 +0300

    create `pcms__users` collection if it  doesn't exist by cloning `users` collection

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"strings"
+
 	"github.com/fatih/color"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 	"github.com/parkuman/pocketcms/ui"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
-	"log"
-	"net/http"
-	"strings"
+	"github.com/pocketbase/pocketbase/models"
 )
 
 const cmsAdminPath = "/cms/"
@@ -34,12 +36,44 @@ func bindStaticAdminUI(e *core.ServeEvent) error {
 	return nil
 }
 
+func addPcmsUsersCollection(app core.App) {
+	collection, err := app.Dao().FindCollectionByNameOrId("users")
+	_, pcms_err := app.Dao().FindCollectionByNameOrId("pcms__users")
+
+	// if "users" collection doesn't exist
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if pcms__users doesn't exist
+	if pcms_err != nil {
+		// Clone the "users" collection to new collection "pcms__users"
+		pcms_users_collection := &models.Collection{
+			Name:       "pcms__users",
+			Type:       models.CollectionTypeAuth,
+			Schema:     collection.Schema,
+			ListRule:   collection.ListRule,
+			ViewRule:  collection.ViewRule,
+			CreateRule: collection.CreateRule,
+			UpdateRule: collection.UpdateRule,
+			DeleteRule: collection.DeleteRule,
+			Options:    collection.Options,
+		}
+
+		// Save the new collection "pcms__users"
+		if err := app.Dao().SaveCollection(pcms_users_collection); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
 func main() {
 	app := pocketbase.New()
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		bindStaticAdminUI(e)
-
+		addPcmsUsersCollection(app)
 		return nil
 	})
 
